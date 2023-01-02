0eb2ca8391743afde15bd8ef4c079d28526fb42a
navikvinay12 <Navik.vinay12@gmail.com>
2023-01-02 18:19:41 +0530

    using sort fn of array and finding 2nd highest and lowest no

--- DSUsingJS.js
+++ DSUsingJS.js
@@ -31,3 +31,43 @@ for (let i = 0; i < numbers.length; i++)
 }
 console.log(`Second largest: ${secondLargest}`);
 console.log(`Second smallest: ${secondSmallest}`);
+//--------------------------------------------------------------------
+// Q2) Write a program in the following steps
+// a. Generates 10 Random 3 Digit number.
+// b. Store this random numbers into a array.
+// c. Then find the 2nd largest and the 2nd smallest element WITH sorting fn in the array.
+let numbers = [];
+for (let i = 0; i < 10; i++) 
+{
+  numbers.push(Math.floor(Math.random() * 900) + 100);
+}
+SecondLargest(numbers)
+SecondSmallest(numbers)     //calling funcitons.
+function SecondLargest(numbers)
+{
+  numbers.sort((a, b) => a - b);
+  let size=numbers.length;
+  for(let i=size-2;i>=0;i--)
+  {
+    if(numbers[i]!=numbers[size-1])
+    {
+      console.log("The Second Largest element is "+ numbers[i]);
+      return;
+    }
+  }
+  console.log("There is No Second Largest element");
+}
+function SecondSmallest(numbers)
+{
+  numbers.sort(function(a, b){return a-b});
+  let size=numbers.length;
+  for(let i=0;i<size-1;i++)
+  {
+    if(numbers[i]!=numbers[i+1])
+    {
+      console.log("The Second Smallest Element is "+ numbers[i+1]);
+      return;
+    }
+  }
+  console.log("There is No Second Smallest element");
+}
